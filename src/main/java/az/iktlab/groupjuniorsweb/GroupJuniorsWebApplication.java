e51dbbfc4138a2482fb69a0790f165ae6494dbf4
Khayal Farziyev <khayal.farziyev@pashabank.az>
2022-03-03 08:57:09 -0800

    test

--- src/main/java/az/iktlab/groupjuniorsweb/GroupJuniorsWebApplication.java
+++ src/main/java/az/iktlab/groupjuniorsweb/GroupJuniorsWebApplication.java
@@ -3,11 +3,19 @@ package az.iktlab.groupjuniorsweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class GroupJuniorsWebApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GroupJuniorsWebApplication.class, args);
+
     }
 
+    @PostConstruct
+    public static void postCons() {
+        System.out.println("Post constructor running");
+    }
 }
